e1ee54f34a141f23ab67451004688b003609a19e
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-12-14 19:37:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is for the chrono gospel</h1>
+      <h3>It will have maps and other things added</h3>
+    </>
+  )
+}
+
+export default App
